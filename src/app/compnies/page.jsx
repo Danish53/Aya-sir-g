3a2362a2115f12ddae912ba3585fc2caa1e93598
Danish53 <daynish05@gmail.com>
2025-07-04 18:27:01 +0500

    aya sir g filter

--- src/app/compnies/page.jsx
+++ src/app/compnies/page.jsx
@@ -1,9 +1,12 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import "./individuals.css";
 import Filterbar from "../components/Filter-bar/Filter-bar";
 import Card from "../components/Personal-card/Card";
 import ButtonComp from "../components/Button-component/ButtonComp";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 
 export default function page() {
   return (
@@ -14,7 +17,26 @@ export default function page() {
             <Filterbar />
           </div>
           <div className="col-lg-8 col-md-12 col-sm-12 ">
-            <ButtonComp />
+            <Suspense
+              fallback={
+                <div className="d-flex flex-wrap gap-3">
+                  {Array.from({ length: 4 }).map((_, index) => (
+                    <div key={index} className="card_div py-3 px-4 skeleton_card">
+                      <Skeleton circle height={100} width={100} />
+                      <Skeleton height={20} width={`60%`} className="mt-3" />
+                      <Skeleton height={15} width={`40%`} className="mt-2" />
+                      <Skeleton height={15} width={`80%`} className="mt-2" />
+                      <Skeleton height={15} width={`50%`} className="mt-2" />
+                      <Skeleton height={15} width={`70%`} className="mt-2" />
+                      <Skeleton height={30} width={`100%`} className="mt-3" />
+                    </div>
+                  ))}
+                </div>
+              }
+            >
+              <ButtonComp />
+            </Suspense>
+
           </div>
         </div>
       </div>
