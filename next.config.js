cb7ba412a90915ff01a4d3314c83224337597409
Danish53 <daynish05@gmail.com>
2025-07-05 01:17:14 +0500

    aya sir G

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-};
+const nextConfig = {};
 
-module.exports = nextConfig;
+export default nextConfig;
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+// };
+
+// module.exports = nextConfig;
 
